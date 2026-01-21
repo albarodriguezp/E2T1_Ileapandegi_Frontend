c0b6a2c5874317006c0cc6fce7371b0f3379c438
ainara <adiazm24@fpsanturtzhilh.eus>
2026-01-21 13:11:35 +0100

    horario en dia detalle, no esta bien todavía

--- src/views/DiaDetalle.vue
+++ src/views/DiaDetalle.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="content">
     <div class="content2">
-        <BarraBusqueda/>
       <h1>Citas del dia {{ dia }}</h1>
+      
+  <div class="grid grid-rows-24">
+  <div
+    v-for="hora in 24"
+    :key="hora"
+    class="fila-hora"
+  >
+    {{ hora }}:00
+    <div v-if="citas[hora]" class="bloque-cita">
+      {{ citas[hora].titulo }}
+    </div>
+  </div>
+  </div>
     </div>
   </div>
 </template>
 
 <script setup>
-    import BarraBusqueda from '@/components/BarraBusqueda.vue'
     import { useRoute } from 'vue-router'
 
     const route = useRoute()
     const dia = route.params.dia
+
+    import { ref } from 'vue'
+    const citas = ref([
+    { titulo: 'Reunión con Juan', inicio: '09:00', fin: '10:00', descripcion: 'Sala 1' },
+    { titulo: 'Llamada cliente', inicio: '11:00', fin: '11:30', descripcion: 'Zoom' },
+    { titulo: 'Revisar informe', inicio: '14:00', fin: '15:30', descripcion: 'Oficina' }
+    ])
 </script>
 
 <style scoped>

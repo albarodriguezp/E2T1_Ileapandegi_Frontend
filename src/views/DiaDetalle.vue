93771d1431be44b09d4e6e26b34c8a1f3818a5ea
ainara <adiazm24@fpsanturtzhilh.eus>
2026-01-22 10:42:34 +0100

    calcular si es miercoles para cambio de horario

--- src/views/DiaDetalle.vue
+++ src/views/DiaDetalle.vue
@@ -14,11 +14,33 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+// Ekari data datuak 
 const route = useRoute()
-const dia = route.params.dia
+const dia = Number(route.params.dia)
 const mes = Number(route.params.mes)
-const ano = route.params.ano
+const ano = Number(route.params.ano)
+
+
+const fechaSeleccionada = computed(() => new Date(ano, mes, dia))
+
+//Azteazkena dela conprobatu beste horario bat duelako
+const esMiercoles = computed(() => fechaSeleccionada.value.getDay() === 3)
+
+// DE BD
+const sillones = [1, 2, 3] 
+//Azteazkenabada hordu bar beztela bezte bat
+const horaInicio = computed(() => esMiercoles.value ? 9 : 10)
+const horaFin = computed(() => esMiercoles.value ? 12.5 : 14.5)
+
+const horasTurno = computed(() => {
+  const slots = []
+  for (let h = horaInicio.value; h < horaFin.value; h += 0.5) {
+    slots.push(h)
+  }
+  return slots
+})
 
 </script>
 <style scoped>
